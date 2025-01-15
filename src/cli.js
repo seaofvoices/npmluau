87bfc374fcd95b822a6042773e50e941692bd1d0
jeparlefrancais <35781636+jeparlefrancais@users.noreply.github.com>
2025-01-15 16:58:40 -0500

    Release 0.1.2 (#12)

--- src/cli.js
+++ src/cli.js
@@ -8,7 +8,7 @@ const { removeFiles } = require('./removeFiles')
 const { createNoCheckLuauRc } = require('./createLuauRc')
 const fs = require('fs').promises
 
-const CLI_VERSION = '0.1.1'
+const CLI_VERSION = '0.1.2'
 
 const getLevelFilterFromVerbosity = (value) => {
   switch (value) {
