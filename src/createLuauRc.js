d266b78724de7fc337b398fb6efff82d81d3fabd
jeparlefrancais <jeparlefrancais21@gmail.com>
2025-01-15 11:29:23 -0500

    remove the @ prefix from the alias

--- src/createLuauRc.js
+++ src/createLuauRc.js
@@ -4,7 +4,7 @@ const path = require('path')
 const NOCHECK_CONFIG = JSON.stringify(
   {
     languageMode: 'nocheck',
-    aliases: { '@pkg': './.luau-aliases' },
+    aliases: { 'pkg': './.luau-aliases' },
   },
   null,
   4
